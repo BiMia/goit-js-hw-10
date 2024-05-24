9c45521e78738ecfd7d0c09785920a0eb1d59767
BiMia <enaselbianca7@gmail.com>
2024-05-24 21:20:19 +0100

    done

---
+++ src/cat-api.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+'live_2ozTZ6HiEGYbXPkDMbWLrUhpPXYBWPDcrYHlVMxvNJ72DVdBfglk79cJL07t9NDJ';
+
+const fetchBreeds = () => {
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+      console.log(response.data);
+      return response.data;
+    })
+    .catch(error => {
+      console.error('API Error');
+    });
+};
+const fetchCatByBreed = breedId => {
+  return axios
+    .get(
+      `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+    )
+    .then(response => {
+      console.log(response.data);
+      return response.data;
+    })
+    .catch(error => {
+      console.error('API Error');
+    });
+};
+
+export { fetchBreeds, fetchCatByBreed };
